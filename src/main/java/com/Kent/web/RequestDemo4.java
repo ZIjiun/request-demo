162778d7158244c5a2069bc2a5780a8a2787b8f4
Zijun <s410126080@gmail.com>
2023-04-06 20:37:48 +0800

    Request 請求轉發

---
+++ src/main/java/com/Kent/web/RequestDemo4.java
@@ -0,0 +1,31 @@
+package com.Kent.web;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+/**
+ * 請求轉發
+ */
+@WebServlet("/req4")
+public class RequestDemo4 extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("demo4...");
+
+        // 存儲資料
+        req.setAttribute("msg", "hello");
+
+        // 請求轉發
+        req.getRequestDispatcher("/req5").forward(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        this.doGet(req, resp);
+    }
+}
---
+++ src/main/java/com/Kent/web/RequestDemo5.java
@@ -0,0 +1,28 @@
+package com.Kent.web;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * 請求轉發
+ */
+@WebServlet("/req5")
+public class RequestDemo5 extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("demo5...");
+
+        // 獲取資料
+        Object msg = req.getAttribute("msg");
+        System.out.println(msg);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        this.doGet(req, resp);
+    }
+}
